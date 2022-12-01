ffc29f412182568e10e433e9d89e2331734a0dd1
VenPoisen <alanmf.pro@gmail.com>
2022-12-01 01:12:20 +0100

    structuring Views path
    Structuring the navegation bar and search field and adding filters when seraching for articles

--- posts/views.py
+++ posts/views.py
@@ -2,21 +2,64 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from posts.models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
-    paginate_by = 3
+    paginate_by = 6
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(publish_post=True)
+        qs = qs.annotate(
+            comments_number=Count(
+                Case(
+                    When(comment__publish_comment=True, then=1)
+                )
+            )
+        )
+
+        return qs
 
 
 class PostSearch(PostIndex):
-    pass
+    template_name = 'posts/search_post.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        term = self.request.GET.get('term')
+
+        if not term:
+            return qs
+
+        qs = qs.filter(
+            Q(title_post__icontains=term) |
+            Q(author_post__first_name__iexact=term) |
+            Q(content_post__icontains=term) |
+            Q(summary_post__icontains=term) |
+            Q(category_post__name_cat__iexact=term)
+        )
+
+        return qs
 
 
 class PostCategory(PostIndex):
-    pass
+    template_name = 'posts/category_post.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        category = self.kwargs.get('category', None)
+
+        if not category:
+            return qs
+
+        qs = qs.filter(category_post__name_cat__iexact=category)
+
+        return qs
 
 
 class PostDetails(UpdateView):
