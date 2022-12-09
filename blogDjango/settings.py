d2c62e9db1cf6e49903d136ca31bff785afea075
VenPoisen <alanmf.pro@gmail.com>
2022-12-09 05:11:30 +0100

    Update settings.py

--- blogDjango/settings.py
+++ blogDjango/settings.py
@@ -143,9 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'templates/static',
-]
+STATICFILES_DIRS = [BASE_DIR / 'templates/static/',]
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
